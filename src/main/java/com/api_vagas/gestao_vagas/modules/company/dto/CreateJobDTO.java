530687f51f90852eb6e510f45da446e762b7dcd3
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-09-14 20:12:42 -0300

    refactor: insert createjobDTO to jobcontroller

---
+++ src/main/java/com/api_vagas/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,13 @@
+package com.api_vagas.gestao_vagas.modules.company.dto;
+
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+
+    private String description;
+    private String benefits;
+    private String level;
+
+}
