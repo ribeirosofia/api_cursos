ea43bcfff55ee6b4889389d6e23f64e11176e2f7
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-09-14 20:22:49 -0300

    refactor: insert candidate password encripted

--- src/main/java/com/api_vagas/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/com/api_vagas/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import com.api_vagas.gestao_vagas.exceptions.UserFoundException;
 import com.api_vagas.gestao_vagas.modules.candidate.CandidateEntity;
 import com.api_vagas.gestao_vagas.modules.candidate.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,19 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity){
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException("Usuário já existe");
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
 
