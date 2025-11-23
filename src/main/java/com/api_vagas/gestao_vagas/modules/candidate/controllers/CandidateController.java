97916531430f5a0cec246eef0670152dd27190d6
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-11-23 20:31:11 -0300

    feat: insert swagger authentication

--- src/main/java/com/api_vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/api_vagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -62,11 +63,12 @@ public class CandidateController {
     @PreAuthorize("hasRole('CANDIDATE')")
     @Tag(name = "Candidato", description = "Informações do candidato")
     @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro.")
-    @ApiResponses(
-          @ApiResponse(responseCode = "200", content = {
-                  @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
-          })
-    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public List<JobEntity> findJobByFilter(@RequestParam String filter){
         return this.listAllJobsByFilterUseCase.execute(filter);
     }
