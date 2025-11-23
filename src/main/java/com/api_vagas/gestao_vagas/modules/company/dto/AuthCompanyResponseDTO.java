4b5d963f424a0011cb8d54b9e8fec7a4a8f43786
ribeirosofia <ribeiro.sofia.sr@gmail.com>
2025-11-23 19:07:20 -0300

    feat: start swagger

---
+++ src/main/java/com/api_vagas/gestao_vagas/modules/company/dto/AuthCompanyResponseDTO.java
@@ -0,0 +1,17 @@
+package com.api_vagas.gestao_vagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCompanyResponseDTO {
+
+    private String access_token;
+    private Long expires_in;
+
+}
--- src/main/java/com/api_vagas/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/api_vagas/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,6 +2,7 @@ package com.api_vagas.gestao_vagas.modules.company.useCases;
 
 
 import com.api_vagas.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.api_vagas.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import com.api_vagas.gestao_vagas.modules.company.repositories.CompanyRepository;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 
 @Service
 public class AuthCompanyUseCase {
@@ -28,7 +30,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
                     throw new UsernameNotFoundException("Username or password incorrect");
@@ -40,13 +42,23 @@ public class AuthCompanyUseCase {
           throw new BadCredentialsException("Credenciais inválidas") ;
       }
 
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+      Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+      var expiresIn = Instant.now().plus(Duration.ofHours(2));
+
        var token = JWT.create().withIssuer("javagas")
                 .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getId().toString())
+                .withExpiresAt(expiresIn)
+                .withClaim("roles", Arrays.asList("COMPANY"))
                 .sign(algorithm);
 
-       return token;
+       var authCompanyResponseDTO =  AuthCompanyResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+
+       return authCompanyResponseDTO;
     }
 
 }
